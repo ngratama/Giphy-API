ea1e9a620c69d68e473044c53703c9be5af62bba
ngratama <95258322+ngratama@users.noreply.github.com>
2022-05-16 14:41:20 -0700

    swiper and key
    added swiper and key

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /*** Variables ***/
 // API Key
-const API_KEY = 'vRgpxNRNKxyIlc1sk9lEifd6kDJZDK57';
+const API_KEY = 'rn4eHCcMx2K0so6O5yWYWRWHzVzgTCSR';
 // limit
-const limit = 25; 
+const limit = 25;
 // Get `input`
 const inputField = document.querySelector('.search-input');
 // Initialize or Default search query
@@ -10,42 +10,44 @@ inputField.value = '';
 
 // Listen to key presses
 inputField.addEventListener('keyup', event => {
-  
+
   if (event.key === 'Enter') {
-    
+
     // Go fetch Giphy API data
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${inputField.value}&limit=${limit}`)
-      .then( response => response.json() )
-      .then( gifs => {
-     
-      // check-check
-      console.log(gifs);
-      console.log(gifs.data);
-      
-      // Get container for data
-      const videoContainer = document.querySelector('.videos');
-      
-      // Loop through the array of data
-      gifs.data.forEach( gif => {
-        
+      .then(response => response.json())
+      .then(gifs => {
+
+        // check-check
+        console.log(gifs);
+        console.log(gifs.data);
+
+        // Get container for data
+        const videoContainer = document.querySelector('.swiper');
+
+        // Loop through the array of data
+        gifs.data.forEach(gif => {
+
           // template 
-          const template  = `
-            <video src="${gif.images.original.mp4}" autoplay loop></video>
+          const template = `
+          <div class="swiper-slide">
+          <video src="${gif.images.original.mp4}"></video>
+        </div>
           `;
-        
+
           // append
           videoContainer.insertAdjacentHTML("afterbegin", template);
-        
+
+        });
+
+
       });
-      
-      
-    });
-    
+
     // Reset value and return cursor focus -- of input field
     inputField.value = '';
     inputField.focus();
-    
+
   }
-  
+
 });
 
